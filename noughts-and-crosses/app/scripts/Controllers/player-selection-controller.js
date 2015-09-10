928401715264b82c8d07e6b8fad95bd7a61a7db8
Andrew Mollett <Andrew.Mollett@tombola.com>
2015-09-10 11:37:46 +0100

    Updated the program to now include 8/8 images for the tiles and also setup the game board

--- noughts-and-crosses/app/scripts/Controllers/player-selection-controller.js
+++ noughts-and-crosses/app/scripts/Controllers/player-selection-controller.js
@@ -1,6 +1,17 @@
 (function () {
     'use strict';
-    angular.module('Tombola.NoughtsAndCrosses').controller('PlayerSelectionController', function(){
-
+    angular.module('Tombola.NoughtsAndCrosses').controller('PlayerSelectionController', function($scope){
+        $scope.player1 = 'human';
+        $scope.player2 = 'human';
+        $scope.playing = false;
+        $scope.createGameboard = function(){
+            $scope.gameboard = {
+                player1 : $scope.player1,
+                player2 : $scope.player2,
+                board : '000000000',
+                playerTurn : 1
+            };
+            $scope.playing = true;
+        };
     });
 })();
