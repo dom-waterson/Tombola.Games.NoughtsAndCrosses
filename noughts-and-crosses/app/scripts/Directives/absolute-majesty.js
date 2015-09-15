331b6bd845cab4b662c7c88db795367211b53073
Andrew Mollett <Andrew.Mollett@tombola.com>
2015-09-15 09:36:34 +0100

    Created an absolute majesty custom directive. Yes it was needed.

---
+++ noughts-and-crosses/app/scripts/Directives/absolute-majesty.js
@@ -0,0 +1,9 @@
+(function () {
+    'use strict';
+    angular.module('Tombola.NoughtsAndCrosses').directive('absoluteMajesty', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'scripts/Directives/absolute-majesty.html'
+        };
+    });
+})();
