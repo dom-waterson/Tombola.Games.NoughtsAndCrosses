312c83e4637cae08ba1bbaa8567fb7594a6977fb
Andrew Mollett <Andrew.Mollett@tombola.com>
2015-09-17 11:18:21 +0100

    Refactored the promise handler such that the code is no longer repeated.

--- noughts-and-crosses/app/scripts/Services/promise-handler.js
+++ noughts-and-crosses/app/scripts/Services/promise-handler.js
@@ -3,28 +3,24 @@
     angular.module('Tombola.NoughtsAndCrosses')
         .service('PromiseHandler', ['$q', '$http', function($q, $http){
             this.prepareGame = function(player1, player2){
-                var deferred = $q.defer();
-                $http.post('http://eutaveg-01.tombola.emea:35000/api/v1.0/newgame',
-                    {
-                        'player1': player1,
-                        'player2': player2
-                    }, {"withCredentials": 'true'})
-                    .then(function(response){
-                        deferred.resolve(response.data);
-                    })
-                    .catch(function(response){
-                        deferred.reject(response.data);
-                    });
-                return deferred.promise;
+                var data = {
+                    'player1': player1,
+                    'player2': player2
+                };
+                return handleRequest(data, 'newgame');
             };
 
             this.makeMove = function(currentPlayer, chosenSquare){
+                var data = {
+                    'playerNumber': currentPlayer,
+                    'chosenSquare': chosenSquare
+                };
+                return handleRequest(data, 'makemove');
+            };
+
+            var handleRequest = function(data, requestType){
                 var deferred = $q.defer();
-                $http.post('http://eutaveg-01.tombola.emea:35000/api/v1.0/makemove',
-                    {
-                        'playerNumber': currentPlayer,
-                        'chosenSquare': chosenSquare
-                    }, {"withCredentials":'true'})
+                $http.post('http://eutaveg-01.tombola.emea:35000/api/v1.0/'+requestType, data , {"withCredentials":'true'})
                     .then(function(response){
                         deferred.resolve(response.data);
                     })
