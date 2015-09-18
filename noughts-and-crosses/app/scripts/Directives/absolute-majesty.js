04892099a2489ef552425a09f13da159e53f9adc
Andrew Mollett <Andrew.Mollett@tombola.com>
2015-09-18 10:57:48 +0100

    Removed the absolute majesty tag ;.;

--- noughts-and-crosses/app/scripts/Directives/absolute-majesty.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    'use strict';
-    angular.module('Tombola.NoughtsAndCrosses').directive('absoluteMajesty', function(){
-        return{
-            restrict: 'E',
-            templateUrl: 'scripts/Directives/absolute-majesty.html'
-        };
-    });
-})();
