85997be87c15d86a82cbcad5420a493126b6ac19
Andrew Mollett <Andrew.Mollett@tombola.com>
2015-09-22 17:33:42 +0100

    Added morgan to the dependencies

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 (function () {
     'use strict';
     var express = require('express');
-    //var moragn = require('morgan');
+    var morgan = require('morgan');
     var app = express();
-    //app.use(morgan('combined'));
+    app.use(morgan('combined'));
     app.use(express.static(process.cwd()+ '/.build/noughts-and-crosses/app', {maxAge:86400000}));
     module.exports = app;
 })();
