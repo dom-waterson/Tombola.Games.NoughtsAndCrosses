8d3df55df542e7ddc7a0aa405a94d621769020c5
Andrew Mollett <Andrew.Mollett@tombola.com>
2015-10-08 11:51:27 +0100

    Added a new audio file to contain all of the game sounds in a single track. Also added a text file to contain data on the sound effect timing.

--- noughts-and-crosses/app/scripts/Directives/sound-directives/sound-directive.js
+++ noughts-and-crosses/app/scripts/Directives/sound-directives/sound-directive.js
@@ -1,13 +1,12 @@
 (function () {
     'use strict';
     angular.module('Tombola.NoughtsAndCrosses')
-        .directive('onClickSound', function(){
+        .directive('tombolaSound', function(){
             return {
                 restrict: 'E',
                 link: function postlink (scope, iElement, iAttrs, controller){
-                    iElement.append(angular.element('<audio><source src="sound/Silly.mp3" type="audio/mp3"/></audio>'));
+                    iElement.append(angular.element('<audio><source src="sound/game-sounds.mp3" type="audio/mp3"/></audio>'));
                 }
             };
-
         });
 })();
