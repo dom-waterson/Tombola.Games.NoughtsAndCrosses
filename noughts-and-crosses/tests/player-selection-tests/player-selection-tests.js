88791d8c762a5b27287390eb5f1405953a730dc8
Andrew Mollett <Andrew.Mollett@tombola.com>
2015-10-12 10:32:57 +0100

    Config files have been created and prepared

---
+++ noughts-and-crosses/tests/player-selection-tests/player-selection-tests.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+    var assert = require('chai').assert;
+    var should = require('chai').should;
+    var playerSelectionService = require('../../app/scripts/player-selection-service');
+    describe('Player selection service tests', function(){
+        beforeEach(function(){
+
+            var me = {};
+
+        });
+    });
+})();
