4fa87314ab64b6deb110c990e99f555ca6e9cee4
Andrew Mollett <Andrew.Mollett@tombola.com>
2015-10-12 16:10:45 +0100

    Written my first unit test! It can fail and succeeds if it should succeed.

--- noughts-and-crosses/tests/player-selection-tests/player-selection-tests.js
+++ noughts-and-crosses/tests/player-selection-tests/player-selection-tests.js
@@ -1,10 +1,17 @@
 (function () {
     'use strict';
     describe('Player selection service tests', function(){
+        var playerSelectionService;
         beforeEach(function(){
+            module('Tombola.NoughtsAndCrosses');
+            inject(function(_PlayerSelectionService_){
+                playerSelectionService = _PlayerSelectionService_;
+            });
+        });
 
-            var me = {};
-
+        it('should have the player defaults set to human', function(){
+            playerSelectionService.players[0].should.equal('human');
+            playerSelectionService.players[1].should.equal('human');
         });
     });
 })();
