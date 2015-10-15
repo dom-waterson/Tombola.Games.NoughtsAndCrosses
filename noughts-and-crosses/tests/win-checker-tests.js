004ff4baccd18fa5b532908cb4c58a329383ec13
Andrew Mollett <Andrew.Mollett@tombola.com>
2015-10-15 14:09:34 +0100

    Added some tests need to add the test functionality

--- noughts-and-crosses/tests/win-checker-tests.js
+++ noughts-and-crosses/tests/win-checker-tests.js
@@ -1,6 +1,32 @@
 (function () {
     'use strict';
     describe('win checker tests', function(){
+        afterEach(function(){
+            module('Tombola.NoughtsAndCrosses', function(){
 
+            });
+        });
+
+        it('should win if gameboard is 111000000 or 222000000', function(){
+
+        });
+
+        it('should win if gameboard is 000111000 or 000222000', function(){
+
+        });
+
+        it('should win if gameboard is 000000111 or 000000222', function(){
+
+        });
+
+        it('should win if gameboard is 100010001 or 200020002', function(){
+
+        });
+
+        it('should win if gameboard is 001010100 or 002020200', function(){
+
+        });
+
+        it('should win if gameboard is ')
     });
 })();
