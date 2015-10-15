835d0f5e28c763ddd72a2f3f1315b303e36a5b38
Andrew Mollett <Andrew.Mollett@tombola.com>
2015-10-15 13:56:39 +0100

    Added mocks to the mock file

--- noughts-and-crosses/mocks/mocks.js
+++ noughts-and-crosses/mocks/mocks.js
@@ -1,1 +1,46 @@
 'use strict';
+var mocks = {
+    gameboardService: {
+        currentGameState: '',
+        playerWinner: '',
+        createGameboard: function(){},
+        gameboard: {
+            player1: 'human',
+            player2: 'human',
+            board: '000000000',
+            playerTurn: 1
+        },
+        playing: false,
+        resetGame: function(){}
+    },
+
+    playerSelectionService: {
+        players: ['human', 'pre-trained'],
+        playing: false,
+        togglePlayer1Type: function(){},
+        togglePlayer2Type: function(){}
+    },
+
+    promiseHandler: {
+        makeMove: function(currentPlayer, chosenSquare){
+            if(currentPlayer === 1){
+                return {gameboard: '100000000'};
+            }
+            else{
+                return {gameboard: '200000000'};
+            }
+        },
+        prepareGame: function(){}
+    },
+
+    gameboardClickerService:{
+        checkOutcome: function(response){},
+        changePlayer: function(){},
+        gameboardClicked: function(squareNumberClicked){}
+    },
+
+    themeChangerService:{
+        theme: 'default',
+        switchTheme: function(){}
+    }
+};
